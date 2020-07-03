a6c4edbd3532a272007b698e8a48930e19276ca4
John Schurk <johnschurk@yahoo.com>
2020-07-02 20:04:08 -0500

    playerController

--- src/main/java/com/reference/icePicks/controllers/playerController.java
+++ src/main/java/com/reference/icePicks/controllers/playerController.java
@@ -1,5 +1,6 @@
 package com.reference.icePicks.controllers;
 
+import com.reference.icePicks.DTOs.GoalieDTO;
 import com.reference.icePicks.DTOs.PlayerDTO;
 import com.reference.icePicks.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,34 +11,82 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
 public class playerController {
 
-@Autowired private PlayerService playerService;
+    @Autowired
+    private PlayerService playerService;
 
-@GetMapping(
+    @GetMapping(
 
-        path = "/roster/player/{lastName}",
-        produces = MediaType.APPLICATION_JSON_VALUE
+            path = "/roster/player/{lastName}",
+            produces = MediaType.APPLICATION_JSON_VALUE
 
 
-
-)
+    )
 
     @ResponseBody
-            public ResponseEntity<PlayerDTO> getPlayerInfo(@PathVariable("lastName") String lastName) {
+    public ResponseEntity<PlayerDTO> getPlayerInfo(@PathVariable("lastName") String lastName) {
 
-    return new ResponseEntity<PlayerDTO>(playerService.retrievePlayerCatalogInfo(lastName), HttpStatus.OK);
-}
+        return new ResponseEntity<PlayerDTO>(playerService.retrievePlayerCatalogInfo(lastName), HttpStatus.OK);
+    }
 
     @PostMapping("/roster/player")
     @ResponseBody
-     public ResponseEntity<PlayerDTO> getPlayer(@RequestBody PlayerDTO playerDTO) {
+    public ResponseEntity<PlayerDTO> catalogPlayer(@RequestBody PlayerDTO playerDTO) {
 
-    return new ResponseEntity<PlayerDTO>(playerService.createPlayerCatalog(playerDTO), HttpStatus.CREATED);
+        return new ResponseEntity<PlayerDTO>(playerService.createPlayerCatalog(playerDTO), HttpStatus.CREATED);
     }
 
+
+    @GetMapping(
+
+            path = "/roster/player/{lastName}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+
+    @ResponseBody
+    public ResponseEntity<GoalieDTO> getGoalieInfo(@PathVariable("lastName") String lastName) {
+
+        return new ResponseEntity<GoalieDTO>(playerService.retrieveGoalieCatalogInfo(lastName), HttpStatus.OK);
+    }
+
+    @PostMapping("/roster/goalie")
+    @ResponseBody
+
+    public ResponseEntity<GoalieDTO> catalogGoalie(@RequestBody GoalieDTO goalieDTO) {
+
+        return new ResponseEntity<GoalieDTO>(playerService.createGoalieCatalog(goalieDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping(
+            path = {
+                    "/roster/player/",
+                    "/roster/player/{playerId}"
+            },
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    public ResponseEntity<List<PlayerDTO>> getPlayer(@PathVariable(required = false, name = "playerId") Long playerId) {
+
+        return new ResponseEntity<List<PlayerDTO>>(playerService.retrievePlayers(playerId), HttpStatus.OK);
+    }
+
+    @GetMapping(
+            path = {
+                    "/roster/goalie",
+                    "/roster/goalie/{goalieId}"
+            },
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    public ResponseEntity<List<GoalieDTO>> getGoalie(@PathVariable(required = false, name = "goalieId") Long goalieId) {
+
+        return new ResponseEntity<List<GoalieDTO>>(playerService.retrieveGoalies(goalieId), HttpStatus.OK);
+    }
 
 
 }
